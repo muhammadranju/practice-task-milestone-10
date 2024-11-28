26cd117cacb84d1f6a66b5032d06e590f7400ecd
Muhammad Ranju <mdranju23@gmail.com>
2024-11-29 00:12:22 +0600

    feat: added Coffee model with mongoose schema
    - Add all fields
    - Using mongoose schema model

---
+++ day_2/backend/models/Coffee.model.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require("mongoose");
+
+const CoffeeSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const Coffee = model("Coffee", CoffeeSchema);
+
+module.exports = Coffee;
